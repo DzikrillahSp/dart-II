64dd7cbe22f39e7a080f031afd9c925fc8211e5e
dzikrillahsukma <80307090+DzikrillahSp@users.noreply.github.com>
2022-03-12 21:52:09 +0700

    Create FutureDelayed.dart

---
+++ FutureDelayed.dart
@@ -0,0 +1,12 @@
+Future delayedPrint(int second, String message){
+  final duration = Duration(seconds : seconds);
+
+  return Future.delayed(duration).then((value) => message);
+}
+  main(List<String> args){
+    print("roger");
+    delayedPrint(2,"pirates"). then((status){
+      print(status);
+    });
+  print("is");
+  }
